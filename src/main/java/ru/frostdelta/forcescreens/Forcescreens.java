7c016d8a44e0dd5f702985543e189266b8f21bd7
FrostDelta123 <pawel.soko2012@yandex.ru>
2018-09-02 11:10:58 +0300

    ScreenShooter mod for BungeeReports plugin. 1.7.10

--- src/main/java/ru/frostdelta/forcescreens/Forcescreens.java
+++ src/main/java/ru/frostdelta/forcescreens/Forcescreens.java
@@ -7,7 +7,6 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.FMLEventChannel;
 import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.common.MinecraftForge;
 import ru.frostdelta.forcescreens.network.Network;
 
--- src/main/java/ru/frostdelta/forcescreens/Utils.java
+++ src/main/java/ru/frostdelta/forcescreens/Utils.java
@@ -1,26 +1,14 @@
 package ru.frostdelta.forcescreens;
 
-import java.lang.reflect.*;
 
 import com.google.common.io.ByteArrayDataOutput;
 import net.minecraft.client.Minecraft;
-import net.minecraft.launchwrapper.Launch;
 import net.minecraft.network.play.client.C17PacketCustomPayload;
 import net.minecraft.util.ChatComponentText;
 
 
 public class Utils {
 
-
-    //private static Minecraft mc;
-    private static boolean isMCP;
-
-
-    // Ебучий костыль (c) Qmaks
-    public static void init() {
-        //mc = Minecraft.getMinecraft();
-        isMCP = (boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
-    }
 
     public static void sendPacket(ByteArrayDataOutput buffer){
         Minecraft mc = Minecraft.getMinecraft();
@@ -29,7 +17,7 @@ public class Utils {
 
     public static void sendMessage(String msg) {
         Minecraft mc = Minecraft.getMinecraft();
-        mc.thePlayer.addChatMessage(new ChatComponentText(("&f[&bAntiCheat&f] " + msg).replace("&", "\u00a7")));
+        mc.thePlayer.addChatMessage(new ChatComponentText(("&f[&AC&f] " + msg).replace("&", "\u00a7")));
     }
 
 }
